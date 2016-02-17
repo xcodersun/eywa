2d54cb312f9702a8a9fc8c53720ace7a11283731
Sea Yo <seayo@vivowares.com>
2016-02-17 09:05:03 -0800

    added test for auth login

---
+++ api_tests/api_auth_test.go
@@ -0,0 +1,48 @@
+// +build integration
+
+package api_tests
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/vivowares/octopus/Godeps/_workspace/src/github.com/bitly/go-simplejson"
+	. "github.com/vivowares/octopus/Godeps/_workspace/src/github.com/smartystreets/goconvey/convey"
+	"github.com/vivowares/octopus/Godeps/_workspace/src/github.com/verdverm/frisby"
+	. "github.com/vivowares/octopus/configs"
+	. "github.com/vivowares/octopus/utils"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func UserLoginPath() string {
+	return fmt.Sprintf("%s/%s", ApiServer, "login")
+}
+
+func TestAuthLogin(t *testing.T) {
+
+	frisby.Global.SetHeader("Content-Type", "application/json").
+		SetHeader("Accept", "application/json")
+
+	Convey("successfully login the user and get the auth token", t, func() {
+		f := frisby.Create("user login").
+			BasicAuth(Config().Security.Dashboard.Username, Config().Security.Dashboard.Password).
+			Get(UserLoginPath()).Send()
+
+		f.ExpectStatus(http.StatusOK).
+			AfterJson(func(F *frisby.Frisby, js *simplejson.Json, err error) {
+			So(len(js.MustMap()["auth_token"].(string)), ShouldBeGreaterThan, 0)
+			ts, _ := js.MustMap()["expires_at"].(json.Number).Int64()
+			exp := time.Unix(MilliSecToSec(ts), MilliSecToNano(ts))
+			So(exp.After(time.Now().Add(-1*time.Minute).Add(Config().Security.Dashboard.TokenExpiry)), ShouldBeTrue)
+		})
+	})
+
+	Convey("refused access without auth token", t, func() {
+		f := frisby.Create("list channels").Get(ListChannelPath()).Send()
+
+		f.ExpectStatus(http.StatusUnauthorized)
+	})
+
+	frisby.Global.PrintReport()
+}
