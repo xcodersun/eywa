22d30c116977050929bca393326700c000c3b1b6
asxcoder <alex@vivowares.com>
2016-03-27 23:18:45 -0700

    Addressed SeaYo's comment
    1. created clang.h to contain golang template
    2. followed model of channel_handler->model->presentor, where model processes data and presentor process response

---
+++ utils/hardware_template_parser.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"strings"
+	"errors"
+	"os"
+	"bufio"
+)
+
+func HardwareTemplateParse(hwTmplPath string, key string, delimStart string, delimEnd string) (string, error) {
+	template := ""
+	openDelim := true
+
+  	hwTmplFile, err := os.Open(hwTmplPath)
+  	if err != nil {
+  		return "", err
+  	}
+  	defer hwTmplFile.Close()
+
+  	scanner := bufio.NewScanner(hwTmplFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, delimStart) && strings.Contains(line, key) {
+			for scanner.Scan() {
+				line = scanner.Text()
+				if strings.HasPrefix(line, delimEnd) {
+					openDelim = false
+					break
+				}
+				if strings.HasSuffix(line, "\\\\n") {
+					line = strings.Replace(line, "\\\\n", "\n", -1)
+				}
+				template += line
+			}
+			break
+		}
+	}
+
+	if openDelim {
+		return "", errors.New("Open delimiter")
+	}
+
+	return template, err;
+}
